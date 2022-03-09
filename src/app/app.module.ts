1fac50e42a388fbb5dccdea96403d8df3b21aaa1
Amanda Lan-Cheong <amanda.lan-cheong@gmail.com>
2022-03-09 11:35:16 +0100

    structure and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormContactComponent } from './components/form-contact/form-contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormContactComponent,
+    FooterComponent,
+    NavbarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
